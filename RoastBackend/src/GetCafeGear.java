30b07e72e24a11e135070f88928a2c3015be906d
Nick Variz <nicholas.variz@service-now.com>
2014-02-10 12:22:27 -0800

    Added Cafe Gear servlet

---
+++ RoastBackend/src/GetCafeGear.java
@@ -0,0 +1,50 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+/**
+ * Servlet implementation class GetCafeGear
+ */
+public class GetCafeGear extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetCafeGear() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String d = request.getParameter("cafe");
+
+		HttpToSqlAdapter driver = new HttpToSqlAdapter();
+		driver.getCafeGear(d);
+		
+		PrintWriter out = response.getWriter();
+		
+		Gson gson = new Gson();
+		String json = gson.toJson(driver);
+		out.write(json);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
